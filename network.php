e27d868496d2b3b0bcd2259cc2f99667ee47651b
Rene <107244670+rene2412@users.noreply.github.com>
2025-02-16 22:27:25 -0800

    Created network.php

---
+++ network.php
@@ -0,0 +1,37 @@
+<?php 
+global $ping, $ip;
+$ping = "PING google.com (2607:f8b0:4005:812::200e) 56 data bytes  
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=1 ttl=250 time=139 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=2 ttl=250 time=161 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=3 ttl=250 time=80.1 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=4 ttl=250 time=44.8 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=5 ttl=250 time=65.2 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=6 ttl=250 time=250 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=7 ttl=250 time=49.9 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=8 ttl=250 time=90.1 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=9 ttl=250 time=53.1 ms
+64 bytes from nuq04s45-in-x0e.1e100.net (2607:f8b0:4005:812::200e): icmp_seq=10 ttl=250 time=57.5 ms
+--- google.com ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 9013ms
+rtt min/avg/max/mdev = 44.763/98.985/249.913/62.369 ms";
+
+$ip = "1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host noprefixroute 
+       valid_lft forever preferred_lft forever
+2: enp1s0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
+    link/ether aa:bb:cc:11:22:33 brd ff:ff:ff:ff:ff:ff
+3: enp2s0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
+    link/ether aa:bb:cc:44:55:66 brd ff:ff:ff:ff:ff:ff
+4: wlp3s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default qlen 1000
+    link/ether 11:22:33:44:55:66 brd ff:ff:ff:ff:ff:ff
+    inet 192.168.50.100/24 brd 192.168.50.255 scope global dynamic noprefixroute wlp3s0
+       valid_lft 60738sec preferred_lft 60738sec
+    inet6 2001:db8::1234:5678:abcd:ef01/64 scope global temporary dynamic 
+       valid_lft 86299sec preferred_lft 14299sec
+    inet6 2001:db8::abcd:ef12:3456:7890/64 scope global dynamic mngtmpaddr noprefixroute 
+       valid_lft 86299sec preferred_lft 14299sec
+    inet6 fe80::abcd:1234:5678:9abc/64 scope link noprefixroute 
+       valid_lft forever preferred_lft forever";
