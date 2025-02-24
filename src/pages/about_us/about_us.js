19eda2851b5020af4f161992bb6bef68517b00b8
herbart hernandez <burnedcard77@gmail.com>
2025-02-24 14:35:14 -0800

    article non component layout complete

--- src/pages/about_us/about_us.js
+++ src/pages/about_us/about_us.js
@@ -3,3 +3,47 @@ import { Footer } from "../../components/Footer/index.js";
 
 const navigation = new Navigation('.navigation__container');
 const footer = new Footer('.footer__container');
+const nav = document.querySelector('.navbar');
+
+const articleTitles= document.querySelectorAll('.article__title');
+const articleLinks = document.querySelectorAll('.article__link');
+
+function updateArticleSideObs(e){
+    articleLinks.forEach((element) => {
+        if(element.innerText === e){
+            element.classList.add('active');
+        }
+        else{
+            element.classList.remove('active')
+        }
+        
+    });
+}
+
+const observer = new IntersectionObserver((items)=>{
+    items.forEach((item)=>{
+        const itemName = item.target.firstElementChild.id;
+        if(item.isIntersecting){
+            console.log(item.target.firstElementChild.innerText);
+            updateArticleSideObs(item.target.firstElementChild.innerText);
+        }
+        else{
+            
+            // console.log(item.target.innerText)
+        }
+    })
+
+}
+,{
+    root: null,
+    rootMargin: "-40% 0px -70% 0px",
+    threshold: 0,
+})
+
+function startView(){
+    articleTitles.forEach((element)=>{
+        observer.observe(element.parentElement);
+    })
+}
+
+startView();
