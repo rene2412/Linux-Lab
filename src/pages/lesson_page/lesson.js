d85d6d1198e262d93e5130628fa7ea4f94481dcc
herbart hernandez <burnedcard77@gmail.com>
2025-02-25 13:55:48 -0800

    lesson and terminal component

--- src/pages/lesson_page/lesson.js
+++ src/pages/lesson_page/lesson.js
@@ -1,3 +1,5 @@
+import VanillaTerminal from "./termC/term.js";
+
 const sidebarBtn = document.querySelector('.top__sidebar');
 const sidebarBtnClose = document.querySelector('.sidebar__button')
 const sidebar = document.querySelector('.sidebar');
@@ -24,9 +26,10 @@ sidebarBtn.addEventListener('click',()=>{
 sidebarBtnClose.addEventListener('click',()=>{
     sidebar.classList.toggle('hidden');
 })
+// this is no longer in use and is reference for structure of the json
 const section = {
     basics:[
-        {section__size:5, section__completed:false,},
+        {section__size:1, section__completed:false,},
         {
             id: 1,
             section: 'basics',
@@ -37,44 +40,7 @@ const section = {
                     <p>blahballhlsldajflalf salfjsafljflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf asfjflalf jflalf jflalf jflalf </p>
                     <code>ls directory</code>
                     <p>blahballhlsldajflalf sjflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf jflalf asfjflalf jflalf jflalf jflalf </p>
-                    
                     `,
-        },
-        {
-            id: 2,
-            section: 'basics',
-            title: 'ls',
-            completed: false,
-            content: `<p>should we do today or i dont even know im just tping</p>
-                    <p>suppposed to be some blahballhlsldajflalf salfjsaflasf</p>
-                    <code>ls directory</code>`,
-        },
-        {
-            id: 3,
-            section: 'basics',
-            title: 'pwd',
-            completed: false,
-            content: `<p>what should we do today or i dont even know im just tping</p>
-                    <p>this is suppposed to be some blahballhlsldajflalf salfjsaflasf</p>
-                    <code>ls directory</code>`,
-        },
-        {
-            id: 4,
-            section: 'basics',
-            title: 'whoami',
-            completed: false,
-            content: `<p>what should we do today or i dont even know im just tping</p>
-                    <p>this is suppposed to be some blahballhlsldajflalf salfjsaflasf</p>
-                    <code>ls directory</code>`,
-        },
-        {
-            id: 5,
-            section: 'basics',
-            title: 'cd',
-            completed: false,
-            content: `<p>what should we do today or i dont even know im just tping</p>
-                    <p>this is suppposed to be some blahballhlsldajflalf salfjsaflasf</p>
-                    <code>ls directory</code>`,
         },
     ],
 }
@@ -91,7 +57,7 @@ class lessonDisplay{
     // LESSONS START FROM 1
     curLesson = 1;
     sectionSize = 0;
-    lessons = [];
+    modules = {};
     
     constructor(container,lessons = null){
         this.container = document.querySelector(container);
@@ -102,10 +68,18 @@ class lessonDisplay{
         this.statusCross = document.querySelector('.lesson__status--cross');
         this.statusCheck = document.querySelector('.lesson__status--check');
 
-        this.getLessons();
+        this.getInfo();
+
+        // this.initListeners();
+        // this.changeSection();
+        // this.updateMeter();
+    }
+    initialize(){
         this.initListeners();
         this.changeSection();
         this.updateMeter();
+        this.render();
+
     }
 
 
@@ -121,8 +95,8 @@ class lessonDisplay{
     }
     render(){
         this.container.replaceChildren();
-        this.container.innerHTML = `<h1 class="lesson__title">${section[this.curSection][this.curLesson]['title']}</h1>
-        <div class="lesson__content">${section[this.curSection][this.curLesson][`content`]}</div>`;
+        this.container.innerHTML = `<h1 class="lesson__title">${this.modules[this.curSection][this.curLesson]['title']}</h1>
+        <div class="lesson__content">${this.modules[this.curSection][this.curLesson][`content`]}</div>`;
     }
     nextLesson= ()=>{
         if(this.curLesson >= this.sectionSize) return;
@@ -135,17 +109,22 @@ class lessonDisplay{
         this.update();
     }
     changeSection(){
-       this.sectionSize = section[this.curSection][0]['section__size'];
+    //    this.sectionSize = section[this.curSection][0]['section__size'];
+       this.sectionSize = this.modules[this.curSection][0]['section__size'];
+       console.log(this.sectionSize);
     }
     updateMeter(){
-       let completedCount = section[this.curSection].reduce((sum,lesson)=>{
+       let completedCount = this.modules[this.curSection].reduce((sum,lesson)=>{
         return (sum + (lesson.completed ? 1 : 0));
        },0)
        let progValue = (completedCount / this.sectionSize)*100;
        this.progBar.value=progValue;
+       if(progValue===100){
+        // change section stustua
+       }
     }
     updateStatus(){
-        if(section[this.curSection][this.curLesson][`completed`] === true){
+        if(this.modules[this.curSection][this.curLesson][`completed`] === true){
             this.statusCheck.classList.remove(`hidden`);
             this.statusCross.classList.add(`hidden`);
         }
@@ -155,28 +134,30 @@ class lessonDisplay{
         }
     }
     toggleLessonComplete=()=>{
-        section[this.curSection][this.curLesson][`completed`] = !section[this.curSection][this.curLesson][`completed`];
+        this.modules[this.curSection][this.curLesson][`completed`] = !this.modules[this.curSection][this.curLesson][`completed`];
         this.updateMeter();
         this.updateStatus();
     }
 
     // this will need to be done in DB or something to keep track of user info
-    // async getInfo(){
-    //     try{
-    //         const request = await fetch('../../testAPI/userInfo.json');
-    //         if(!request.ok){
-    //             throw new Error('Could not load user info');
-    //         }
-    //         const data = await request.json();
-    //         console.log(data.);
-    //     }        
-    //     catch(error){
-    //         console.log(`error ${error}`);
-    //     }
-    // }
+    async getInfo(){
+        try{
+            const request = await fetch('../../testAPI/userInfo.json');
+            if(!request.ok){
+                throw new Error('Could not load user info');
+            }
+            const data = await request.json();
+            // at 0 simply refers to a certain user
+            this.curLesson = data[0].lesson;
+            this.curSection= data[0].section;
+            this.getLessons();
+        }        
+        catch(error){
+            console.log(`error ${error}`);
+        }
+    }
     
     //fetch the lesson after user info
-    // SETUP MY GETTER MAKE EVERTHING RUN AND BUILD AFTER THE GET LESSON FETCHES ELSE LOAD
     async getLessons(){
         try{
             const request = await fetch('../../testAPI/lessons.json');
@@ -184,12 +165,18 @@ class lessonDisplay{
                 throw new Error('Could not load lesson');
             }
             const data = await request.json();
-            console.log(data[this.curSection]);
-            return data[this.curSection];
+            this.modules = data;
+            console.log(this.modules)
+            this.initialize();
+            // return data[this.curSection];
         }        
         catch(error){
             console.log(`error ${error}`);
         }
+    }
+    // we should send back this lesson and this
+    async postInfo(){
+
     }
 
     async postLessons(){
@@ -198,4 +185,9 @@ class lessonDisplay{
 };
 
 const lessonDisplayController = new lessonDisplay('.lesson');
-lessonDisplayController.render();
+// lessonDisplayController.render();
+
+const terminal = new VanillaTerminal({
+    apiEndpoint:'../../../api_commands.php',
+});
+terminal.mount('#terminal__container');
