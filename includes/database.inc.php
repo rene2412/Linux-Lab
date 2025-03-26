f8e29ff292b0ded7a1e6b1735636a10171cc098b
Junior <Junior@Anas-MacBook.local>
2025-03-26 14:00:43 -0700

    sql now on cloud

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,14 +1,15 @@
 <?php
-$host = "localhost";
-$dbname = "Linux_Lab";
+$host = "34.94.191.154"; // Cloud SQL Public IP
+$dbname = "Linux_Lab";  
 $dbusername = "root";
-$dbpassword = "";
+$dbpassword = "pandas_024";
 
 try {
 	//create a php data object called php that represents the parameters to connect to the database
 	$pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword); 
 	//->setAttribute will set the php object to look for errors in the connection
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully!"; // 
 } catch(PDOException $e) {  
 	die("Connection Failed: " . $e->getMessage());
 } 
