fd3522d84a25a591dbf65248faf6f3ca1128a9f3
herba1 <burnedcard77@gmail.com>
2025-03-27 21:09:51 -0700

    nav rework

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,19 @@
 export default class Navigation{
+    LINKS = [
+        {title:'Dashboard',link:'#',svg:'../../pages/assets/SVGs/Control Panel.svg', tag:'#'},
+        {title:'Home',link:'../../pages/landing_page/landing_page.html',tag:'landing_page.html',svg:'../../pages/assets/SVGs/Home.svg'},
+        {title:'Terminal',link:'../../pages/lesson_page/lesson.html',tag:'lesson.html',svg:'../../pages/assets/SVGs/Console.svg'},
+        {title:'About',link:'../../pages/about_us/about_us.html',tag:'about_us.html',svg:'../../pages/assets/SVGs/Info Squared.svg'},
+        {title:'Contact',link:'../../pages/contact_page/contact.html',tag:'contact.html',svg:'../../pages/assets/SVGs/Address Book.svg'},
+    ]
+    path=""
+
+    // need to set this up
+    isLoggedIn = false;
     constructor(container){
         this.container = document.querySelector(container);
-
+        this.fullPath = window.location.pathname;
+        this.path = this.fullPath.substring(this.fullPath.lastIndexOf('/')+1)
         this.render();
         this.setElements();
         this.setListeners();
@@ -29,6 +41,23 @@ export default class Navigation{
     }
 
     createSidebarTemplate(){
+        const listElem = this.LINKS.map((elem)=>{
+            let cssClass= '';
+            if(!this.isLoggedIn && elem.title === 'Dashboard') return '';
+            if(this.path === elem.tag){
+                cssClass = 'link--active';
+            }
+            
+            return(`
+                <li class="navbar__link ${cssClass} "><a href="${elem.link}">${elem.title}</a><Img src="${elem.svg}" alt="${elem.title}"/></li>
+                `);
+        })
+
+        let cssClass = '';
+        let loginText = 'Login'
+        // user should go here
+        if(this.isLoggedIn){cssClass='--auth'; loginText=`<span class="green">@</span>herb`};
+
         const sidebar = document.createElement('div');
         sidebar.classList.add('sidebar');
         sidebar.classList.add('sidebar--close');
@@ -36,22 +65,18 @@ export default class Navigation{
             <div class="sidebar__top">
                 <div class="sidebar__header">
                     <h2 class="sidebar__logo">Linux-Lab</h2>
-                    <button type="button" class="button sidebar__button sidebar__button--close">X</button>
+                    <button type="button" class="button sidebar__button sidebar__button--close"><img src="../../pages/assets/SVGs/Close.svg" /></button>
                 </div>
                 <nav class="sidebar__links">
                     <ul>
-                        <li class="navbar__link"><a href="../../pages/landing_page/landing_page.html">Home</a></li>
-                        <li class="navbar__link"><a href="#">Learn More</a></li>
-                        <li class="navbar__link"><a href="../../pages/lesson_page/lesson.html">Try It</a></li>
-                        <li class="navbar__link"><a href="../../pages/about_us/about_us.html">About Us</a></li>
-                        <li class="navbar__link"><a href="../../pages/contact_page/contact.html">Contact Us</a></li>
-                        <li class="navbar__link"><a href="../../pages/login/login.php">login</a></li>
+                    ${listElem.join('')}
                     </ul>
                 </nav>
             </div>
             <div class="sidebar__bottom">
-                <button type="button">
-                    <img src="../assets/SVGs/cog-outline.svg " class="svg" alt="cog">
+                <button class="sidebar__button--user${cssClass}"><a href="../../pages/login/login.php">${loginText}</a></button>
+                <button type="button sidebar__button--settings">
+                    <img class="sidebar__img--settings" src="../assets/SVGs/Settings.svg " class="svg" alt="cog">
                 </button>
             </div>
         `;
@@ -59,6 +84,17 @@ export default class Navigation{
     }
 
     createNavbarTemplate(){
+        const listElem = this.LINKS.map((elem)=>{
+            let cssClass= '';
+            if(!this.isLoggedIn && elem.title === 'Dashboard') return '';
+            if(this.path === elem.tag){
+                cssClass = 'link--active--desktop';
+            }
+            
+            return(`
+                <li class="navbar__link navbar--link--desktop  ${cssClass} "><a href="${elem.link}">${elem.title}</a></li>
+                `);
+        })
         const nav = document.createElement('nav');
         nav.classList.add('navbar');
         nav.innerHTML = `
@@ -68,11 +104,7 @@ export default class Navigation{
             </div> 
             <div class="navbar__right">
                 <ul class="navbar__links">
-                    <li class="navbar__link"><a href="../../pages/landing_page/landing_page.html">Home</a></li>
-                    <li class="navbar__link"><a href="#">Learn More</a></li>
-                    <li class="navbar__link"><a href="../../pages/lesson_page/lesson.html">Try It</a></li>
-                    <li class="navbar__link"><a href="../../pages/about_us/about_us.html">About Us</a></li>
-                    <li class="navbar__link"><a href="../../pages/contact_page/contact.html">Contact Us</a></li>
+                ${listElem.join('')}
                 </ul>
             </div> 
         `;
