037a0461208298446eb06bd94123f5a9ec046b71
Junior <Junior@anas-macbook.mynetworksettings.com>
2025-04-04 20:53:12 -0700

    sql on digital ocean

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,9 +1,9 @@
 <?php
-$host = "34.94.191.154"; // Cloud SQL Public IP
+$host = "137.184.234.64"; // Cloud SQL Public IP
+//$host = "localhost";
 $dbname = "Linux_Lab";  
 $dbusername = "root";
-$dbpassword = "pandas_024";
-
+$dbpassword = "";
 try {
 	//create a php data object called php that represents the parameters to connect to the database
 	$pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword); 
