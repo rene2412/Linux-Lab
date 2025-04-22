2ea9c76971633bc2a0f744c59b60f7de48e8dcb1
herbart hernandez <burnedcard77@gmail.com>
2025-04-21 20:05:05 -0700

    init

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -2,12 +2,12 @@
 $host = "localhost"; 
 $dbname = "Linux_Lab";  
 $dbusername = "root";
-$dbpassword = "pandas_024";
+$dbpassword = "Pandas_1"; 
+
 try {
-	//create a php data object called php that represents the parameters to connect to the database
-	$pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname", $dbusername, $dbpassword); 
-	//->setAttribute will set the php object to look for errors in the connection
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {  
-	die("Connection Failed: " . $e->getMessage());
-} 
+    die("Connection Failed: " . $e->getMessage());
+}
+?>
