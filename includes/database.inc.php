1a92e94465e7fa5947575e553a06a9d1abe417a1
herba1 <burnedcard77@gmail.com>
2025-04-27 13:13:04 -0700

    test

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -9,5 +9,6 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {  
     die("Connection Failed: " . $e->getMessage());
+    die("Connection Failed: " . $e->getMessage());
 }
 ?>
