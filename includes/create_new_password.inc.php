493467a348ac2a3d03b13b5d4d05386921177f76
herbart hernandez <burnedcard77@gmail.com>
2025-06-05 15:27:28 -0700

    page styling

--- includes/create_new_password.inc.php
+++ includes/create_new_password.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
- require_once "config_session.inc.php";
+require_once "config_session.inc.php";
 
 $selector = $_GET["selector"];
 $validator = $_GET["validator"];
@@ -10,19 +10,43 @@ if (empty($selector) || empty($validator)) {
 }
 else {
     if (ctype_xdigit($selector) !== false && ctype_xdigit($validator) !== false) {
-     ?>
-        <form action="reset_password_submit.inc.php" method="POST">
-            <input type="hidden" name="selector" value="<?php echo $selector ?>">
-            <input type="hidden" name="validator" value="<?php echo $validator ?>">
-            <input type ="password" name="pwd" placeholder="Enter a new password..">
-            <input type ="password" name="rpwd" placeholder="Re-enter your new password..">
-            <button type ="submit" name = "reset-password-submit">Reset Password</button>
-        </form>
-        <?php
-        //check for reset password errors;
-        if (!empty($_SESSION['rwpd'])) {
-            echo '<p style="color:red;">' . $_SESSION['rpwdErrors'] . '</p>';
-            unset($_SESSION['rpwdErrors']); 
-        }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/pages/login/login.css">
+    <title>Reset Password</title>
+</head>
+<body>
+    <div class="auth__container">
+        <div class="auth__card animation--popup">
+            <div class="auth__top">
+                <h2 class="auth__top__logo"><a href="../src/pages/landing_page/landing_page.html">Linux-Lab</a></h2>
+            </div>
+            <div class="auth__card__content auth__card__content--reset">
+                <h3>Create New Password</h3>
+                <form action="reset_password_submit.inc.php" method="POST">
+                    <input type="hidden" name="selector" value="<?php echo $selector ?>">
+                    <input type="hidden" name="validator" value="<?php echo $validator ?>">
+                    <label for="pwd" class="input__label">New Password:</label>
+                    <input required type="password" class="input--single" name="pwd" id="pwd" placeholder="Enter a new password..">
+                    <label for="rpwd" class="input__label">Confirm Password:</label>
+                    <input required type="password" class="input--single" name="rpwd" id="rpwd" placeholder="Re-enter your new password..">
+                    <button type="submit" name="reset-password-submit" class="styled-button">Reset Password</button>
+                </form>
+                <?php
+                if (!empty($_SESSION['rwpd'])) {
+                    echo '<p style="color:red;">' . $_SESSION['rpwdErrors'] . '</p>';
+                    unset($_SESSION['rpwdErrors']); 
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php
     }
 }
