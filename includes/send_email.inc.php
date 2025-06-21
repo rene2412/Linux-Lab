f998e5a45f8bc183601a4b3191f6a89bb205aea9
Rene <hrene2412@gmail.com>
2025-06-21 14:23:32 -0700

    herb updates and email send

--- includes/send_email.inc.php
+++ includes/send_email.inc.php
@@ -2,12 +2,15 @@
 session_start();
 require __DIR__ . '/../vendor/autoload.php';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\SMTP;
+
+use \SendGrid\Mail\Mail;
+
 
 if (isset($_SESSION["send_email"])) {
-    $email = $_SESSION["send_email"];
 
+    $userEmail = $_SESSION["send_email"];
     $selector = bin2hex(random_bytes(8));
     $token = random_bytes(32);
 
@@ -15,7 +18,6 @@ if (isset($_SESSION["send_email"])) {
 
     $url = "https://linux-lab.live/includes/create_new_password.inc.php?selector=" . $selector . "&validator=" . bin2hex($token);
     $expires = date("U") + 1800;
-    $userEmail = $_SESSION["send_email"];
     
     // SQL Queries
     $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
@@ -24,7 +26,7 @@ if (isset($_SESSION["send_email"])) {
     if (!$stmt) {
         die("Debug: SQL DELETE failed: " . implode(", ", $pdo->errorInfo()));
     } else {
-        $stmt->execute([$email]);
+        $stmt->execute([$userEmail]);
     }
 
     $sql = "INSERT INTO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) 
@@ -35,38 +37,30 @@ if (isset($_SESSION["send_email"])) {
         die("Debug: SQL INSERT failed: " . implode(", ", $pdo->errorInfo()));
     } else {
         $hashedToken = password_hash($token, PASSWORD_DEFAULT); 
-        $stmt->execute([$email, $selector, $hashedToken, $expires]);
-}
+        $stmt->execute([$userEmail, $selector, $hashedToken, $expires]);
+             error_log("✅ INSERT executed");
+    }
 
     // Sending the email
-    $mail = new PHPMailer(true);
+   // $mail = new PHPMailer(true);
 
+
+    $email = new Mail();
+    $email->setFrom("no-reply@linux-lab.live", "Linux Lab");
+    $email->setSubject("Reset Your Password");
+    $email->addTo($userEmail);
+    $email->addContent("text/html", "Here is your reset link: <a href='$url'>$url</a>");   
+    
+    
     try {
-
-        // Server settings
-        $mail->isSMTP();
-        $mail->SMTPAuth = true;
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-        $mail->Username = 'linuxlab012@gmail.com';
-        $mail->Password = 'ayfp eofz khas nmki'; 
-
-        // Recipients
-        $mail->setFrom($userEmail);
-        $mail->addAddress($userEmail);
-
-        // Content
-        $mail->Subject = "Reset Your Password";
-        $mail->Body = 'Here is your password reset link: </br>';
-        $mail->Body .= '<a href="' . $url . '">' . $url . '</a></p>';
-
-        $mail->send();
-        header("Location: email_sent.html");
-    } catch (Exception $e) {
-        error_log("Mailer Error: {$mail->ErrorInfo}");
-    }
-} else {
-    //header("Location: ../index.php");
+    $response = $sendgrid->send($email);
+    error_log("✅ Email status code: " . $response->statusCode());
+    header("Location: email_sent.html");
     exit();
 }
+
+   catch (Exception $e) {
+        error_log("SendGrid Error: " . $e->getMessage());
+    }
+}
+
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     
 	$username = $_POST["username"];	
