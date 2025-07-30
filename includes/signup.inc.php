3f50925d1bc665b5e4dd0a7e93201d96c3be8529
Rene <hrene2412@gmail.com>
2025-07-29 22:55:23 -0700

    fixed path after email validation

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,75 +1,53 @@
 <?php
 session_start();
 
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-	$username = $_POST["username"];	
-    $email = $_POST["email"];	
-    $pwd = $_POST["pwd"];
+  
+   $username = $_POST["username"];
+   $email = $_POST["email"];  
+   $pwd = $_POST["pwd"];
 
-    try {
-        require_once 'database.inc.php';
-        require_once 'signup_model.inc.php';
-        require_once 'signup_contr.inc.php';	
-	 
-		$errors = [];
-    
-		if (is_input_empty($username, $pwd, $email)) {
-		$errors["empty_input"] = "Fill In All Fields!";		
-		}
 
-		if (!is_email_valid($email)) {
-            $errors["invalid_email"] = "Invalid E-Mail!";		
-        }
-    
-        if (username_is_taken($pdo, $username)) {
-            $errors["username_taken"] = "Username is taken. Please choose another!";		
-        }
-		if (is_username_too_long($username )) {
-            $errors["invalid_username"] = "Username can't exceed past 20 characters";		
-		}
-        if (email_is_registered($pdo, $email)) {
-            $errors["email_taken"] = "Email already registered. Please choose another!";		
-        }
+       require_once 'database.inc.php';
+       require_once 'signup_model.inc.php';
+       require_once 'signup_contr.inc.php';   
+   
+       $errors = [];
+  
+       if (is_input_empty($username, $pwd, $email)) {
+       $errors["empty_input"] = "Fill In All Fields!";    
+       }
 
-		if (is_passsword_valid($pwd) === false) {
-			$errors["password_ invalid"] = "Invalid Password!";
-		}
 
-		session_start();
-	if ($errors) {
-		$_SESSION["errors_signup"] = $errors; 
-		 header("Location: ../src/pages/login/signup.php");	
-		 die();
-		}
-	    $result = create_user($pdo, $pwd, $username, $email);
-		$newSessionId = session_create_id();		
-	    $sessionId = $newSessionId . "_" . $result["id"];
-		session_id($sessionId);
-	
-		$_SESSION["user_id"] = $result["id"];
-		$_SESSION["user_username"] = htmlspecialchars($result["username"]);
-		$_SESSION["last_regeneration"] = time();
-		$user_id = $_SESSION["user_id"];
-		//for current module
-		$sql = $pdo->prepare("INSERT INTO user_progress (user_id, lesson_id, lessons_completed, current_lesson, current_module) VALUES (?, ?, ?, ?, ?)");
-		$sql->execute([$user_id, 0, 0, 0, 'The Basics']);
-		//set user is logged
-		$stmt = $pdo->prepare("UPDATE users SET is_logged_in = 1 WHERE id = ?");
-        $stmt->execute([$_SESSION["user_id"]]);
+       if (!is_email_valid($email)) {
+           $errors["invalid_email"] = "Invalid E-Mail!";      
+       }
+  
+       if (username_is_taken($pdo, $username)) {
+           $errors["username_taken"] = "Username is taken. Please choose another!";       
+       }
+       if (is_username_too_long($username )) {
+           $errors["invalid_username"] = "Username can't exceed past 20 characters";      
+       }
+       if (email_is_registered($pdo, $email)) {
+           $errors["email_taken"] = "Email already registered. Please choose another!";       
+       }
 
-		if ($_SESSION["user_id"] !== null) {
-			require_once 'cookies.inc.php';
-			$json_response = json_encode($response);
-			setcookie('user_info', $json_response, 0, "/"); // Expires when browser closes
-		}
-	 
-	 header("Location: ../src/pages/landing_page/landing_page.html");
-	 $pdo = null;
-	 $stmt = null;	 
-	 die();
 
-	} catch (PDOException $e) {
-        echo "Caught exception: " . $e->getMessage();
-    }
+       if (is_passsword_valid($pwd) === false) {
+           $errors["password_ invalid"] = "Invalid Password!";
+       }
+
+
+   if ($errors) {
+       $_SESSION["errors_signup"] = $errors;
+        header("Location: ../src/pages/login/signup.php");
+        die();
+       }
+   //If vetting is good, then its ready to check if email is valid
+   $_SESSION["pwd"] = $pwd;
+   $_SESSION["user_email"] = $email;  
+   $_SESSION["user_username"] = $username;
+   header("Location: verifyEmail.php");
 }
