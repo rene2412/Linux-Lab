315e6105347be164761c2195304a4e868d209238
herba1 <burnedcard77@gmail.com>
2025-08-06 23:03:21 -0700

    dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className=" w-5 text-3xl">hello world</h1>
+    </>
+  );
+}
+
+export default App;
