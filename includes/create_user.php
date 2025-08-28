fc1a2c180ab78e0f6374521586d53df765bfd82d
Rene <hrene2412@gmail.com>
2025-08-27 22:24:52 -0700

    buggy main

--- includes/create_user.php
+++ includes/create_user.php
@@ -32,7 +32,7 @@ try {
 			setcookie('user_info', $json_response, 0, "/"); // Expires when browser closes
 		}
 	 
-	 header("Location: ../src/pages/landing_page/landing_page.html");
+	 header("Location: ../src/pages/dashboard/dashboard.html");
 	 $pdo = null;
 	 $stmt = null;
      $pwd = null;	 
